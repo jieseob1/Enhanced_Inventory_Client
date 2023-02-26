c48b5a624529bb1206865a7269bf7c89c70d80f6
jieseob1 <jieseob1@gmail.com>
2023-02-26 10:18:48 +0900

    Initialize webpack configuration

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,44 @@
 const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // to generate an index html file based on our template file
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    // defined entry point for our application as './src/index.js'
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['**/*', '!static-files*'],
+        }),      
+        new HtmlWebpackPlugin({
+        title: 'Production',
+        }),
+    ],
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, './dist'), //output path is dist
 
- module.exports = {
-   entry: {
-     app: './src/index.js',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Production',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                    }
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,16 @@
 const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
- module.exports = merge(common, {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   devServer: {
-     static: './dist',
-   },
- });
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map', //original: inline-source-map
+    devServer: {
+        static: './dist',
+        hot: true, // what is hot??
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+
+});
